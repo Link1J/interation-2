246117fcb03bbb16d3f33a8a43caa704cd945f0e
Jinsung1022 <57107998+Jinsung1022@users.noreply.github.com>
2021-04-06 15:46:38 -0400

    Made qix.py cleaner and added comments

--- Qix.py
+++ Qix.py
@@ -10,24 +10,12 @@ class Qix:
     Class representing Qix
     """
     def __init__(self, pos: Vector2):
-        self.qix_pos = pos
-        self.qix_mov = Vector2(0.01, 0.0)
-        self.x = pos[0]
-        self.y = pos[1]
-        self.win = None
-        self.x_vel = 0
-        self.y_vel = 0
-        self.x_sign = 1
-        self.y_sign = 1
-        self.vel = 0.1
-        self.count = 0
-
-    def set_surface(self, win):
-        self.win = win
-
-    def update(self, facing: float):
-        pos = Vector2(self.x, self.y)
-        drawing.qix_update(pos, facing)
+        self.qix_pos = pos                  # Current Position of qix
+        self.qix_mov = Vector2(0.01, 0.0)   # Makes the sticks move in circle
+        self.vel = 0                        # Overall velocity of the qix
+        self.x_vel, self.y_vel = 0, 0       # x and y velocity of the qix
+        self.x_sign, self.y_sign = 1, 1     # 1 for positive sign, -1 for negative sign
+        self.count = 0                      # Count so that qix doesnt change direction every time qix.move() is called
 
     def bound(self, pos: Vector2):
         if pos.x < 0:
@@ -42,19 +30,26 @@ class Qix:
     def angle(self, mov: Vector2) -> float:
         return math.atan2(mov.y, mov.x) * (180 / math.pi)
 
-    def draw(self, size: float):
+    def draw(self, size: float) -> None:
+        """
+        Draws the qix
+        :param size: Size of the qix
+        :return: None
+        """
         self.qix_pos = self.qix_pos + self.qix_mov
         self.bound(self.qix_pos)
-        self.qix_mov.rotate_ip(10)# random.randint(0, 20))
+        self.qix_mov.rotate_ip(10)  # random.randint(0, 20))
         drawing.qix(self.qix_pos, self.angle(self.qix_mov), size)
 
-    def test_move(self):
-        self.qix_pos[0] += 0.001
-        print (self.qix_pos[0])
-
-    def move(self):
+    def move(self) -> None:
+        """
+        Makes qix move in random direction and random speed for certain period of time.
+        :return: None
+        """
         self.count += 1
+        # Changes direction and speed every 50th time move() is called.
         if (self.count % 50) == 0:
+            # Speed of the qix can be from 0.003 to 0.006
             self.vel = random.randrange(3, 6)
             self.vel = self.vel/1000
             # Gets a random direction from degree 1 to 360
@@ -79,13 +74,18 @@ class Qix:
                 self.y_sign = 1
 
             hyp = self.vel
+            # Calculates x_velocity and y_velocity required in order to move in the selected angle.
             self.x_vel = self.x_sign * (math.cos(math.radians(angle))) * hyp
             self.y_vel = self.y_sign * (math.sin(math.radians(angle))) * hyp
 
-        #if (0 < (self.qix_pos[0] + self.x_vel) < 1) and (0 < (self.qix_pos[1] + self.y_vel) < 1):
+        # if (0 < (self.qix_pos[0] + self.x_vel) < 1) and (0 < (self.qix_pos[1] + self.y_vel) < 1):
         self.qix_pos[0] += self.x_vel
         self.qix_pos[1] += self.y_vel
 
-    def get_pos(self):
+    def get_pos(self) -> Vector2:
+        """
+        Returns the current position of the qix
+        :return: Current position of qix.
+        """
         pos = Vector2(self.x, self.y)
         return pos
